fe6c1accca809cf52333c21a227d9c322d59577e
* <anonymousrhino01@gmail.com>
2020-09-12 11:28:59 -0500

    switching to mac?!

--- Rhinos-bot/events/messageUpdate.js
+++ Rhinos-bot/events/messageUpdate.js
@@ -1,11 +1,16 @@
 const Discord = require('discord.js')
 const { Events } = require('../node_modules/discord.js/src/util/Constants.js')
 module.exports = async (client, oldMessage, newMessage, message, guild) => {
+const channelMentioner = /<#([0-9]+)>/g
+const channelMentioner2 = /([0-9]+)/g
 if (oldMessage.author.bot === true) return;
 	if (guild === null) return;
+	
 	const settings = client.getSettings(oldMessage.guild)
+	if (oldMessage.guild.id === '714576528289300522') console.log(settings)
 	if (settings.messageLogging !== "true") return;
 	if (oldMessage.content === newMessage.content) return
+	const channelMention = channelMentioner.exec(settings.messageLogChannel) || channelMentioner2.exec(settings.messageLogChannel)
 	let embed = new Discord.RichEmbed()
 .setAuthor(oldMessage.author.tag, oldMessage.author.avatarURL)
   .setThumbnail(oldMessage.author.avatarURL)
@@ -14,6 +19,6 @@ if (oldMessage.author.bot === true) return;
   .setDescription(`**Before: **  ${oldMessage.content.slice(0, 950)}\n**After: ** ${newMessage.content.slice(0, 950)}`)
   .setTimestamp()
   .setFooter(oldMessage.author.id)
-	newMessage.guild.channels.find(c => c.name === settings.messageLogChannel).send(embed).catch(console.error)
-}
+	 oldMessage.guild.channels.get(channelMention[1]).send(embed).catch(console.error) || oldMessage.guild.channels.find(c => c.name === settings.messageLogChannel).send(embed).catch(console.error)
+channelMentioner.exec(settings.messageLogChannel)}
 
