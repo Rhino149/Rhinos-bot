5a01c8b3572960cea0618e999505e0c79820c874
Rhino149 <56201800+Rhino149@users.noreply.github.com>
2020-09-26 20:03:03 -0500

    Delete error.js

--- Rhinos-bot/events/error.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (client, error) => {
-  client.logger.log(`An error event was sent by Discord.js: \n${JSON.stringify(error)}`, "error");
-client.channels.get('746858943317409813').send(`An error event was sent by Discord.js: \n${JSON.stringify(error)}`).then(msg => {
-msg.channel.send(error)
-})
-};
