80d9d46fbe1311467ed039c8d87571e01a098d36
Rhino149 <56201800+Rhino149@users.noreply.github.com>
2020-09-26 20:03:51 -0500

    Delete guildCreate.js

--- Rhinos-bot/events/guildCreate.js
+++
@@ -1,7 +0,0 @@
-// This event executes when a new guild (server) is joined.
-
-module.exports = (client, guild) => {
-console.log(`${guild.id}, ${guild.name}, ${guild.owner.user.tag}`)
-  client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot with ${guild.memberCount} users. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
-  client.channels.get('717139782924238889').send(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot with ${guild.memberCount} users. Owner: ${guild.owner.user.tag} (${guild.owner.user.id}).`);
-};
